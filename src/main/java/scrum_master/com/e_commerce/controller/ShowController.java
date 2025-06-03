7f34518605bdaa012952f4233664f7c6c0f8f78e
MisaelRosal <kevin.rosales1@catolica.edu.sv>
2025-06-03 16:37:23 -0600

    Aun falta show y edit para la parte de producto

--- src/main/java/scrum_master/com/e_commerce/controller/ShowController.java
+++ src/main/java/scrum_master/com/e_commerce/controller/ShowController.java
@@ -1,12 +1,6 @@
 package scrum_master.com.e_commerce.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
-public class CreateController {
-    @GetMapping("")
-    public String createproducto() {
-        return "createproductos/create";
-    }
+public class ShowController {
 }
